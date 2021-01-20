3bf745bda0764ce355e0552141bcce2bc71540b3
Wldudd <wldudd1211@naver.com>
2021-01-21 03:04:27 +0900

    오른쪽 화면의 TV에 대기하는 DB 환자데이터 출력

--- hos_simulator.py
+++ hos_simulator.py
@@ -39,7 +39,7 @@ nowDatetime=now.strftime('%Y-%m-%d %H:%M:%S')
 
 
 # 예약자 목록 TV화면 (오른쪽 위 화면)
-# 임시 환자
+# 임시 환자 리스트 삽입
 ppList=(
     ('Park', '19990124', '남자', '알레르기', nowDatetime),
     ('Cho', '19681211', '여자', '편두통', nowDatetime),
@@ -52,7 +52,7 @@ ppList=(
 #cursor.execute("INSERT INTO p_list(pname, pbirth, psex, psym, pdate) \
 #    VALUES(?,?,?,?,?)", ('Lee', '20000101', '남자', '화상', nowDatetime))
 
-
+#기본 출력라벨 생성
 wait_list1 = Label(tv, text = "대기번호", font = ('arial 18 bold'))
 wait_list1.place(x=70, y=45)
 
@@ -65,6 +65,45 @@ wait_list1.place(x=260, y=45)
 wait_list1 = Label(tv, text = "증상", font = ('arial 18 bold'))
 wait_list1.place(x=340, y=45)
 
+#cursor.execute("DELETE FROM p_list WHERE id = ?", (1,))
+
+#대기하는 DB 환자데이터 출력
+count = 0
+
+id_list = []
+patients = []
+birthday = []
+gender = []
+hurt = []
+nowdate = []
+
+sql = "SELECT * FROM p_list"
+res = cursor.execute(sql)
+for r in res:
+    count += 1
+    ids = r[0]
+    names = r[1]
+    births = r[2]
+    gens = r[3]
+    hurts = r[4]
+    dates = r[5]
+    id_list.append(ids)
+    patients.append(names)
+    birthday.append(births)
+    gender.append(gens)
+    hurt.append(hurts)
+    nowdate.append(dates)
+    num_list = Label(tv, text = count, font = ('arial 16 bold'))
+    num_list.place(x=110, y=60+count*30)
+    name_list = Label(tv, text = names, font = ('arial 16'))
+    name_list.place(x=190, y=60+count*30)
+    gen_list = Label(tv, text = gens, font = ('arial 16'))
+    gen_list.place(x=265, y=60+count*30)
+    hurt_list = Label(tv, text = hurts, font = ('arial 16'))
+    hurt_list.place(x=330, y=60+count*30)
+
+#for p_list in cursor.execute("SELECT * FROM p_list"):
+#    print(p_list)
 
 # 진료 상황 시뮬레이터 (왼쪽 화면)
 
