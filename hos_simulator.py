d65db468c374843cc040959790ed6105097cc515
Wldudd <wldudd1211@naver.com>
2021-01-21 14:50:10 +0900

    오른쪽 화면의 TV에 실시간 진료 대기목록 업데이트

--- hos_simulator.py
+++ hos_simulator.py
@@ -2,6 +2,9 @@
 from tkinter import *
 import datetime #날짜 생성에 필요한 패키지
 import sqlite3
+import random
+import time
+import threading
 
 # 통합화면
 root = Tk()
@@ -44,7 +47,7 @@ ppList=(
     ('Park', '19990124', '남자', '알레르기', nowDatetime),
     ('Cho', '19681211', '여자', '편두통', nowDatetime),
     ('An', '19880507', '여자', '외상', nowDatetime),
-    #('JY', '19991211', '여자', '두통', nowDatetime),
+    ('JY', '19991211', '여자', '두통', nowDatetime),
 )
 #cursor.executemany("INSERT INTO p_list(pname, pbirth, psex, psym, pdate) \
 #    VALUES(?,?,?,?,?)", ppList)
@@ -52,47 +55,27 @@ ppList=(
 #cursor.execute("INSERT INTO p_list(pname, pbirth, psex, psym, pdate) \
 #    VALUES(?,?,?,?,?)", ('Lee', '20000101', '남자', '화상', nowDatetime))
 
-#기본 출력라벨 생성
-wait_list1 = Label(tv, text = "대기번호", font = ('arial 18 bold'))
-wait_list1.place(x=70, y=45)
-
-wait_list1 = Label(tv, text = "이름", font = ('arial 18 bold'))
-wait_list1.place(x=185, y=45)
-
-wait_list1 = Label(tv, text = "성별", font = ('arial 18 bold'))
-wait_list1.place(x=260, y=45)
-
-wait_list1 = Label(tv, text = "증상", font = ('arial 18 bold'))
-wait_list1.place(x=340, y=45)
-
-#cursor.execute("DELETE FROM p_list WHERE id = ?", (1,))
-
-#대기하는 DB 환자데이터 출력
-count = 0
-
 id_list = []
 patients = []
-birthday = []
 gender = []
 hurt = []
-nowdate = []
 
 sql = "SELECT * FROM p_list"
 res = cursor.execute(sql)
+count = 0
+max = 0
+
 for r in res:
     count += 1
     ids = r[0]
     names = r[1]
-    births = r[2]
     gens = r[3]
     hurts = r[4]
-    dates = r[5]
     id_list.append(ids)
     patients.append(names)
-    birthday.append(births)
     gender.append(gens)
     hurt.append(hurts)
-    nowdate.append(dates)
+
     num_list = Label(tv, text = count, font = ('arial 16 bold'))
     num_list.place(x=110, y=60+count*30)
     name_list = Label(tv, text = names, font = ('arial 16'))
@@ -102,10 +85,66 @@ for r in res:
     hurt_list = Label(tv, text = hurts, font = ('arial 16'))
     hurt_list.place(x=330, y=60+count*30)
 
-#for p_list in cursor.execute("SELECT * FROM p_list"):
-#    print(p_list)
+def update_db():
+    #기본 출력라벨 생성
+    wait_list1 = Label(tv, text = "대기번호", font = ('arial 18 bold'))
+    wait_list1.place(x=70, y=45)
+
+    wait_list1 = Label(tv, text = "이름", font = ('arial 18 bold'))
+    wait_list1.place(x=185, y=45)
+
+    wait_list1 = Label(tv, text = "성별", font = ('arial 18 bold'))
+    wait_list1.place(x=260, y=45)
+
+    wait_list1 = Label(tv, text = "증상", font = ('arial 18 bold'))
+    wait_list1.place(x=340, y=45)
+
+    #리스트 초기화
+    id_list.clear()
+    patients.clear()
+    gender.clear()
+    hurt.clear()
+
+    #DB의 대기환자 출력
+    count = 0
+    res = cursor.execute("SELECT * FROM p_list")
+    for r in res:
+        count += 1
+        ids = r[0]
+        names = r[1]
+        gens = r[3]
+        hurts = r[4]
+        id_list.append(ids)
+        patients.append(names)
+        gender.append(gens)
+        hurt.append(hurts)
+        num_list = Label(tv, text = count, font = ('arial 16 bold'))
+        num_list.place(x=110, y=60+count*30)
+        name_list = Label(tv, text = names, width=5, font = ('arial 16'))
+        name_list.place(x=190, y=60+count*30)
+        gen_list = Label(tv, text = gens, width=4, font = ('arial 16'))
+        gen_list.place(x=265, y=60+count*30)
+        hurt_list = Label(tv, text = hurts, width=7, font = ('arial 16'))
+        hurt_list.place(x=330, y=60+count*30)
+        if count > 7:
+            break
+    print(id_list[0], names)
+    cursor.execute("DELETE FROM p_list WHERE id = ?", (id_list[0],))
+    
+    #랜덤진료시간 생성
+    doctor_time = random.randrange(2000, 10000)
+
+    #최대 대기인원 max
+    global max
+    max += 1
+    if max < 6:
+        print("{} 실행".format(doctor_time))
+        tv.after(doctor_time, update_db)
+    
+update_db()
 
 # 진료 상황 시뮬레이터 (왼쪽 화면)
 
-conn.close()
+
 root.mainloop()
+conn.close()
